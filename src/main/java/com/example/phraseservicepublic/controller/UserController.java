3c2dceb3c9808ca2d6cfe1bff9b5c7612a5d3ce2
FastMakc <122231638+FastMakc@users.noreply.github.com>
2023-03-05 21:13:36 +0300

    Update UserController.java

--- src/main/java/com/example/phraseservicepublic/controller/UserController.java
+++ src/main/java/com/example/phraseservicepublic/controller/UserController.java
@@ -1,14 +1,16 @@
-package com.example.phraseservicepublic.controller;
+package ru.mycompany.phrase.controller;
 
-import com.example.phraseservicepublic.domain.api.user.login.LoginReq;
-import com.example.phraseservicepublic.domain.api.user.publicPhrases.PublicPhraseReq;
-import com.example.phraseservicepublic.domain.api.user.registration.RegistrationReq;
-import com.example.phraseservicepublic.domain.response.Response;
-import com.example.phraseservicepublic.service.UserService;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ru.mycompany.phrase.domain.api.user.login.LoginReq;
+import ru.mycompany.phrase.domain.api.user.publicPhrase.PublicPhraseReq;
+import ru.mycompany.phrase.domain.api.user.registration.RegistrationReq;
+import ru.mycompany.phrase.domain.response.Response;
+import ru.mycompany.phrase.service.user.UserService;
+
 
 @Slf4j
 @RestController
@@ -18,6 +20,8 @@ public class UserController {
 
     private final UserService userService;
 
+
+
     @GetMapping("/hello")
     public String hello() {
         String hello = "Hello, phrase-service! Version: 1.0.0";
@@ -25,31 +29,40 @@ public class UserController {
         return hello;
     }
 
+
+
     @GetMapping("/getMyPhrases")
     public ResponseEntity<Response> getMyPhrases(@RequestHeader final String accessToken) {
 
-        log.info("START getMyPhrases, accessToken: {}", accessToken);
+        log.info("START endpoint getMyPhrases, accessToken: {}", accessToken);
         ResponseEntity<Response> resp = userService.getMyPhrases(accessToken);
-        log.info("END endpoint getMyPhrases, response:{}", resp);
+        log.info("END endpoint getMyPhrases, response: {}", resp);
         return resp;
     }
 
+
+
     @PostMapping("/publicPhrase")
     public ResponseEntity<Response> publicPhrase(@RequestHeader final String accessToken, @RequestBody final PublicPhraseReq req) {
+
         log.info("START endpoint publicPhrase, accessToken: {}, request: {}", accessToken, req);
         ResponseEntity<Response> resp = userService.publicPhrase(req, accessToken);
         log.info("END endpoint publicPhrase, response: {}", resp);
         return resp;
     }
 
+
+
     @PostMapping("/login")
     public ResponseEntity<Response> login(@RequestBody final LoginReq req) {
+
         log.info("START endpoint login, request: {}", req);
         ResponseEntity<Response> resp = userService.login(req);
         log.info("END endpoint login, response: {}", resp);
         return resp;
-
     }
+
+
 
     @PostMapping("/registration")
     public ResponseEntity<Response> registration(@RequestBody final RegistrationReq req) {

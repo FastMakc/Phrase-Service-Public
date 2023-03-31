4630c7dc02e6e0bfc9b3e5ca07ab129eb03c5692
FastMakc <122231638+FastMakc@users.noreply.github.com>
2023-03-31 16:06:31 +0300

    Update AuthorizationFilter.java

--- src/main/java/ru/mycompany/phrase/filter/AuthorizationFilter.java
+++ src/main/java/ru/mycompany/phrase/filter/AuthorizationFilter.java
@@ -20,7 +20,6 @@ import static org.springframework.http.HttpStatus.BAD_REQUEST;
 @Slf4j
 public class AuthorizationFilter extends OncePerRequestFilter {
 
-
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 

78706a5beb3566ef11ac37b7a13df6231098c167
Max Fedorenko <122231638+FastMakc@users.noreply.github.com>
2023-06-23 19:22:51 +0300

    Update SubscriptionDaoImpl.java

--- src/main/java/ru/mycompany/phrase/dao/communication/SubscriptionDaoImpl.java
+++ src/main/java/ru/mycompany/phrase/dao/communication/SubscriptionDaoImpl.java
@@ -1,5 +1,6 @@
 package ru.mycompany.phrase.dao.communication;
 
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;

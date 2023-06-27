3ac5bfad653cc98608e7f74f3458d642e7a525f1
Max Fedorenko <122231638+FastMakc@users.noreply.github.com>
2023-06-27 09:11:40 +0300

    Update UserDaoImpl.java

--- src/main/java/ru/mycompany/phrase/dao/user/UserDaoImpl.java
+++ src/main/java/ru/mycompany/phrase/dao/user/UserDaoImpl.java
@@ -1,5 +1,6 @@
 package ru.mycompany.phrase.dao.user;
 
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;

01e1162bc8cd1a4cf682137dd04451c1ca5171d2
Max Fedorenko <122231638+FastMakc@users.noreply.github.com>
2023-07-06 07:21:56 +0300

    Update RegExp.java

--- src/main/java/ru/mycompany/phrase/domain/constant/RegExp.java
+++ src/main/java/ru/mycompany/phrase/domain/constant/RegExp.java
@@ -2,6 +2,7 @@ package ru.mycompany.phrase.domain.constant;
 
 public class RegExp {
 
+    
     public final static String nickname = "^[a-zA-Z0-9а-яА-Я. _-]{4,15}$";
     public final static String partNickname = "^[a-zA-Z0-9а-яА-Я. _-]{3,15}$";
     public final static String password = "^[a-zA-Z0-9а-яА-Я.,:; _?!+=/'\\\\\"*(){}\\[\\]\\-]{8,100}$";
